c62563301d2242b4a74f20a1fbe386e304a5f26e
Hamid Ahmetovic <hamid.ahmetovic@mhp.com>
2019-10-07 08:19:12 +0200

    event listener missing commit

--- src/main/java/com/example/foosball/foosball/listener/FoosballEventListener.java
+++ src/main/java/com/example/foosball/foosball/listener/FoosballEventListener.java
@@ -1,4 +1,21 @@
 package com.example.foosball.foosball.listener;
 
+import com.example.foosball.foosball.service.FoosballService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
 public class FoosballEventListener {
+
+    @Autowired
+    private FoosballService foosballService;
+
+    @EventListener
+    public void onDisconnectEvent(final SessionDisconnectEvent event) {
+        log.info("Client {} disconnected", event.getUser());
+    }
 }
